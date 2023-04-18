7eb3b86e957a430b783be86784120c732e3208dd
Ricardo Martinez <ricko62291@gmail.com>
2023-04-17 19:04:16 -0500

    initial commit

---
+++ environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase:  {
+        apiKey: "AIzaSyCSlCQ6e9uj94ipPgcfvLqLatNomEGV6TM",
+        authDomain: "juntosadelante-uno.firebaseapp.com",
+        projectId: "juntosadelante-uno",
+        storageBucket: "juntosadelante-uno.appspot.com",
+        messagingSenderId: "208551775708",
+        appId: "1:208551775708:web:dc754cad126be2f069d319",
+        measurementId: "G-K3ZK53V1RX"
+    }
+  };
+  
